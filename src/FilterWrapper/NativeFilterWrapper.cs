3bde045994fd8231e70b87f64a70833cc4647877
Anton <41597155+antontidev@users.noreply.github.com>
2022-02-11 01:39:41 +0500

    Fixed problem with filters

--- src/FilterWrapper/NativeFilterWrapper.cs
+++ src/FilterWrapper/NativeFilterWrapper.cs
@@ -8,17 +8,19 @@ namespace OdinGames.EcsLite.Native.NativeOperations.FilterWrapper
     public struct NativeFilterWrapper : IDisposable
     {
         private ReadOnlyNativeWrappedData<int> _filter;
-
+        private int _length;
+        
         public void Init(EcsFilter filter)
         {
             _filter = filter.GetRawEntities().WrapToReadOnlyNative();
+            _length = filter.GetEntitiesCount();
         }
 
         public int this[int index]
             => _filter.Array[index];
 
         public int Count()
-            => _filter.Array.Length;
+            => _length;
         
         public void Dispose()
         {
